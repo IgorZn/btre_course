c017c03c1a1a24a386613e2b868ea83dda4a2a82
Igor Znamensky <igor.znamensky@gmail.com>
2019-08-21 10:25:30 +0300

    Done.

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 # Create your views here.
@@ -15,10 +16,27 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # check if user already has made inquiry
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contact = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contact:
+                messages.error(request, 'You have already made inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing_id=listing_id, listing=listing, name=name, email=email, message=message,
                           user_id=user_id, phone=phone)
 
         contact.save()
 
-        message.success(request, 'Your request has been submitted, a realtor will get back to you soon')
-        return redirect('listing/'+listing_id)
+        # Send email
+        send_mail(
+            'Subject here',
+            'Here is the message. ' + listing,
+            'igor.znamensky@gmail.com',
+            [realtor_email, 'igor_znamenskii@epam.com'],
+            fail_silently=False,
+        )
+
+        messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+        return redirect('/listings/'+listing_id)
