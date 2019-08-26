687e515861feba3d0e888e5a5cf0f64f96798992
IgorZn <igor.znamensky@gmail.com>
2019-08-26 22:10:45 +0300

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -149,5 +149,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'igor.znamensky@gmail.com'
-EMAIL_HOST_PASSWORD = 'nbhwxglcnzzizugx'
+EMAIL_HOST_PASSWORD = 'nbhwxglcnzzizugxx'
 EMAIL_USE_TLS = True
